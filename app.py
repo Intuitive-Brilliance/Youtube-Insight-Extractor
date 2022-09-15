199ea6777931781f34ab3cf5229df06b6b93ddf6
Atyab Hakeem <88573121+Intuitive-Brilliance@users.noreply.github.com>
2022-09-16 01:45:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,12 +24,21 @@ import pymongo
 
 import boto3
 
+def log(file_object, log_message):
+    now = datetime.datetime.now()
+    date = now.date()
+    current_time = now.strftime("%H:%M:%S")
+    file_object.write(str(date) + "/" + str(current_time) + "\t\t" + log_message +"\n")
+
 
 app = Flask(__name__)
 # Route to display the home page
 @app.route('/',methods=['GET'])
 @cross_origin()
 def homePage():
+    file_object = open("Homepage_log/homepage_log.txt", 'a+')
+    log_message = 'Render template - index.html called'
+    log(file_object, log_message)
     return render_template("index.html")
 
 # Route to show the compiled data in a web UI
@@ -40,6 +49,9 @@ def index():
         try:
             #Opens Chrome
             def openchrome(url):
+                file_object = open("Index_log/openchrome_log.txt", 'a+')
+                log_message = 'Openchrome called'
+                log(file_object, log_message)
                 driver = webdriver.Chrome(executable_path='chromedriver.exe')
                 driver.get(url)
 
@@ -63,11 +75,15 @@ def index():
                     new_height = driver.execute_script("return document.documentElement.scrollHeight")
                     last_height = new_height
                     count += 1
+                log_message = 'Openchrome successfully executed'
+                log(file_object, log_message)
                 return driver
 
             # Scrapes channel
             def scrape_channel(driver, url):
-
+                file_object = open("Index_log/scrapechannel_log.txt", 'a+')
+                log_message = 'Scrapechannel called'
+                log(file_object, log_message)
                 titles_se = driver.find_elements(By.ID, "video-title")
                 titles = [titles_se[i].text for i in range(50)]
                 vids = pt.Channel(url)[0:50]
@@ -78,14 +94,21 @@ def index():
                 channel_name = driver.find_elements(By.XPATH, '//*[@id="text"]')[2].text
                 links = [titles_se[i].get_attribute("href") for i in range(50)]
                 driver.quit()
+                log_message = 'Channel:{} Scrapechannel successfully executed'.format(url)
+                log(file_object, log_message)
                 return (titles, views, images, links, channel_name)
 
             # Scrapes Videos
             def scrape_vids(driver, url):
+                file_object = open("Index_log/scrapevids_log.txt", 'a+')
+                log_message = 'Video:<{}> Scrapevids called'.format(url)
+                log(file_object, log_message)
                 try:
                     likes = driver.find_elements(By.XPATH, '//*[@id="text"]')[2].text
                 except:
                     likes = 'NA'
+                    log_message = 'Video:<{}> Likes not found'.format(url)
+                    log(file_object, log_message)
                 try:
                     duration_se = driver.find_elements(By.XPATH, "//span[@class='ytp-time-duration']")[0].text
                     temp = time.strptime(duration_se, '%M:%S')
@@ -95,15 +118,20 @@ def index():
                         duration = pt.YouTube(url).length
                     except:
                         duration = 'NA'
+                        log_message = 'Video:<{}> Duration not found'.format(url)
+                        log(file_object, log_message)
                 try:
-                    comments_no = \
-                    driver.find_elements(By.XPATH, '//*[@id="count"]/yt-formatted-string')[0].text.split(' ')[0]
+                    comments_no = driver.find_elements(By.XPATH, '//*[@id="count"]/yt-formatted-string')[0].text.split(' ')[0]
                 except:
                     comments_no = 'NA'
+                    log_message = 'Video:<{}> Comment number not found'.format(url)
+                    log(file_object, log_message)
                 try:
                     comments = driver.find_elements(By.XPATH, '//*[@id="contents"]')
                 except:
                     comments = 'NA'
+                    log_message = 'Video:<{}> Comments not found'.format(url)
+                    log(file_object, log_message)
                 cmnts = dict()
                 try:
                     cmnts_cluster = comments[0].text.split('\n')
@@ -115,8 +143,12 @@ def index():
                                 cmnts[cmnts_cluster[i - 1] + '<' + str(i) + '>'] = cmnts_cluster[i + 1]
                     if len(cmnts) == 0:
                         cmnts = {'NA': 'NA'}
+                        log_message = 'Video:<{}> Comment not found'.format(url)
+                        log(file_object, log_message)
                 except:
                     cmnts = {'NA': 'NA'}
+                    log_message = 'Video:<{}> Comment not found'.format(url)
+                    log(file_object, log_message)
                 driver.quit()
                 return (likes, comments_no, cmnts, duration)
 
@@ -134,11 +166,11 @@ def index():
             duration = list()
 
             # Limiting the data to save time
-            titles = titles[:10]
-            views = views[:10]
-            images = images[:10]
-            links = links[:10]
-            duration = duration[:10]
+            titles = titles[:1]
+            views = views[:1]
+            images = images[:1]
+            links = links[:1]
+            duration = duration[:1]
 
             count = 0
             for i in links:
@@ -185,12 +217,17 @@ def index():
 def insights():
     if request.method == 'POST':
         try:
+            file_object = open("Insights_log/insights_log.txt", 'a+')
+            log_message = 'Insights function called'
+            log(file_object, log_message)
             df_in = df[['views', 'likes', 'comments_no', 'duration']]
             df_in['length_of_title'] = [len(i) for i in df.titles]
 
             likes = df_in.likes.to_list()
             count=0
 
+            log_message = 'Data cleaning started'
+            log(file_object, log_message)
             # Data cleaning
             for lks in likes:
                 try:
@@ -202,6 +239,7 @@ def insights():
                         likes[count] = str(act_lks)
                     except:
                         likes[count] = 'NA'
+
                 count += 1
             df_in['likes']=likes
 
@@ -221,8 +259,13 @@ def insights():
             df_in['views']=views
 
             ProfileReport(df_in).to_file(os.getcwd()+'\\templates\\report.html')
+            log_message = 'Profile report successfully generated'
+            log(file_object, log_message)
             return render_template('report.html')
         except Exception as e:
+            file_object = open("Insights_log/insights_log.txt", 'a+')
+            log_message = 'Insights function called but something went wrong. Error:{}'.format(str(e))
+            log(file_object, log_message)
             return 'Seomething went wrong '+str(e)
 
 
@@ -232,10 +275,16 @@ def insights():
 def savetocloud():
     if request.method == 'POST':
         try:
+            file_object = open("Savetocloud_log/savetocloud_log.txt", 'a+')
+            log_message = 'Save to cloud function called'
+            log(file_object, log_message)
             df_cloud = df[['links', 'titles', 'views', 'images', 'likes', 'comments_no', 'top_comment', 'duration']]
 
             lst_of_tuples = list(df_cloud.itertuples(index=False, name=None))
             try:
+                file_object = open("Savetocloud_log/mysql.txt", 'a+')
+                log_message = 'Trying to insert data into Azure Mysql'
+                log(file_object, log_message)
                 # Connecting to mysql and creating new table for channel and inserting data
                 mydb = conn.connect(host='', user='', passwd='',port=3306, ssl_ca="")
                 cursor = mydb.cursor()
@@ -252,10 +301,21 @@ def savetocloud():
                     cursor.execute(query)
                     mydb.commit()
                 mydb.close()
+
                 outcome1 = 'Successfully uploaded to Azure MySQL Database!!!'
-            except Exception as outcome1:
-                pass
+
+
+                log(file_object, 'Successfully uploaded to Azure MySQL Database')
+
+            except Exception as e:
+                outcome1 = str(e)
+                log_message = 'Trying to insert data into Azure Mysql but something went wrong. Error:{}'.format(str(outcome1))
+                log(file_object, log_message)
+
             try:
+                file_object = open("Savetocloud_log/mongodb.txt", 'a+')
+                log_message = 'Trying to insert data into MongoDB Atlas'
+                log(file_object, log_message)
                 # Connecting to MongoDB and storing data
                 client = pymongo.MongoClient("")
                 db = client.test
@@ -275,11 +335,22 @@ def savetocloud():
 
                 client.close()
                 outcome2= 'Successfully uploaded to MongoDB Atlas!!!'
-            except Exception as outcome2:
-                pass
+
+
+                log(file_object, 'Successfully uploaded to MongoDB Atlas')
+
+            except Exception as e:
+                outcome2 = str(e)
+                log_message = 'Trying to insert data into MongoDB Atlas but something went wrong. Error:{}'.format(str(outcome2))
+                log(file_object, log_message)
+
             outcome = 'Outcome:'+'\n' + str(outcome1) + '\n' + str(outcome2)
+
             return render_template('outcome.html',outcome =outcome )
         except Exception as e:
+            file_object = open("Savetocloud_log/savetocloud_log.txt", 'a+')
+            log_message = 'Save to cloud function called but something went wrong. Error:{}'.format(str(e))
+            log(file_object, log_message)
             return 'Something went wrong: '+str(e)
 
 # Route to download videos to cloud
@@ -288,6 +359,9 @@ def savetocloud():
 def downlaodvid():
     if request.method == 'POST':
         try:
+            file_object = open("Downloadtocloud_log/downloadtocloud_log.txt", 'a+')
+            log_message = 'Download to cloud function called'
+            log(file_object, log_message)
             links = df.links.to_list()
             titles = df.titles.to_list()
 
@@ -296,35 +370,67 @@ def downlaodvid():
             try:
                 #channel_name = 'Telusko'
                 # Connecting to S3 Bucket
-                s3 = boto3.resource(service_name='s3', region_name='us-east-1', aws_access_key_id='',aws_secret_access_key='')
+                file_object = open("Downloadtocloud_log/s3bucket_log.txt", 'a+')
+                log_message = 'Connecting to AWS S3 Bucket'
+                log(file_object, log_message)
+                s3 = boto3.resource(service_name='s3', region_name='', aws_access_key_id='',aws_secret_access_key='')
                 bucket_name = channel_name.lower().replace(' ', '-')
                 s3.create_bucket(ACL='private', Bucket=bucket_name)
-                for ele in range(2):
+                for ele in range(len(links)):
                     try:
+
+                        log_message = 'Video:<{}> Downloading...'.format(titles[ele])
+                        log(file_object, log_message)
+
                         # Downloading video
                         vid = pt.YouTube(links[ele])
                         path_vid = vid.streams.filter(file_extension='mp4', res="240p").first().download()
+
+
+                        log_message = 'Video:<{}> Download sucessful'.format(titles[ele])
+                        log(file_object, log_message)
                         # path_vid = os.getcwd()+'\\'+titles[ele]+'.mp4'
                     except:
+
+                        log_message = 'Video:<{}> Download Failed. Tryng next video'.format(titles[ele])
+                        log(file_object, log_message)
                         continue
                     try:
+
+                        log_message = 'Video:<{}> Uploading...'.format(titles[ele])
+                        log(file_object, log_message)
+
                         # Uploading video
                         s3.Bucket(bucket_name).upload_file(Filename=path_vid, Key=titles[ele] + '.mp4')
+
+
+                        log_message = 'Video:<{}> Upload successful'.format(titles[ele])
+                        log(file_object, log_message)
                     except:
+
+                        log_message = 'Video:<{}> Upload failed'.format(titles[ele])
+                        log(file_object, log_message)
                         continue
 
                 # Uploading csv
                 s3.Bucket(bucket_name).upload_file(Filename=channel_name + '.csv', Key=channel_name + '.csv')
-
+    
                 outcome = 'Videos successfully downloaded to AWS S3 Bucket!!!'
-            except Exception as outcome:
-                pass
+
+            except Exception as e:
+                outcome = str(e)
+                file_object = open("Downloadtocloud_log/s3bucket_log.txt", 'a+')
+                log_message = 'Connection to AWS S3 Bucket failed. Error{}'.format(str(outcome))
+                log(file_object, log_message)
 
             return render_template('outcome.html', outcome = 'Outcome: ' + str(outcome))
         except Exception as e:
+            file_object = open("Downloadtocloud_log/downloadtocloud_log.txt", 'a+')
+            log_message = 'Download to cloud function called but something went wrong. Error:{}'.format(str(e))
+            log(file_object, log_message)
             return 'Something went wrong: ' + str(e)
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8061, debug=True)
+    app.run(host='127.0.0.1', port=8071, debug=True)
 	#app.run(debug=True)
